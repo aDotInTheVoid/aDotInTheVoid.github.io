66e8075d981a9560e3c5fbe1269198191c60bab0
Alona Enraght-Moony <code@alona.page>
2025-10-07 23:09:18 +0100

    Bump tree-sitters

--- src/highlight.rs
+++ src/highlight.rs
@@ -3,21 +3,24 @@ use tree_sitter_highlight::{HighlightConfiguration, Highlighter, HtmlRenderer};
 pub fn highlight(lang: &str, source: &str) -> String {
     let mut conf = match lang {
         "rust" => HighlightConfiguration::new(
-            tree_sitter_rust::language(),
-            tree_sitter_rust::HIGHLIGHT_QUERY,
+            tree_sitter_rust::LANGUAGE.into(),
+            "rust",
+            tree_sitter_rust::HIGHLIGHTS_QUERY,
             "",
             "",
         )
         .unwrap(),
         "toml" => HighlightConfiguration::new(
-            tree_sitter_toml::language(),
-            tree_sitter_toml::HIGHLIGHT_QUERY,
+            tree_sitter_toml::LANGUAGE.into(),
+            "toml",
+            tree_sitter_toml::HIGHLIGHTS_QUERY,
             "",
             "",
         )
         .unwrap(),
         "c" => HighlightConfiguration::new(
-            tree_sitter_c::language(),
+            tree_sitter_c::LANGUAGE.into(),
+            "c",
             tree_sitter_c::HIGHLIGHT_QUERY,
             "",
             "",
@@ -26,10 +29,11 @@ pub fn highlight(lang: &str, source: &str) -> String {
 
         "asciiart" => return source.to_owned(),
 
+        "asm" => return source.to_owned(),
         _ => panic!("Unknown language: {}", lang),
     };
 
-    let names = conf.query.capture_names().to_owned();
+    let names: Vec<String> = conf.names().iter().map(|i| (*i).to_owned()).collect();
     conf.configure(&names);
 
     let mut highlighter = Highlighter::new();
@@ -44,8 +48,11 @@ pub fn highlight(lang: &str, source: &str) -> String {
         .collect();
 
     let mut html = HtmlRenderer::new();
-    html.render(highlights, source.as_bytes(), &|i| classes[i.0].as_bytes())
-        .unwrap();
+    html.render(highlights, source.as_bytes(), &|hl, out| {
+        let class = classes[hl.0].as_bytes();
+        out.extend_from_slice(class);
+    })
+    .unwrap();
 
     String::from_utf8(html.html).unwrap()
 }
